48df7b1c87419efb2bfe2d555af2512257f79d4e
JorWo <42422209+JorWo@users.noreply.github.com>
2021-01-30 22:51:04 -1000

    Add files via upload

--- scriptB.js
+++ scriptB.js
@@ -1,4 +1,4 @@
-
+bevAmount = [];
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
@@ -10,9 +10,15 @@ if (this.readyState == 4 && this.status == 200) {
         document.getElementById("itemName").innerHTML = mydata.beverages[x].name;
         document.getElementById("itemPrice").innerHTML = "$"+(mydata.beverages[x].price).toFixed(2);
         document.getElementById("itemDesc").innerHTML = mydata.beverages[x].desc;
+        document.getElementById("itemAmount").innerHTML = bevAmount[x];
+        for (i=0; i<2;i++) {
+        document.getElementsByClassName("addToCartBut")[i].style.visibility = "visible";
+        }
     }
 
     for (i=0;i<Object.keys(mydata.beverages).length;i++) {
+        bevAmount[i] = 0;
+
         var objTo1 = document.getElementById("selScroll");
         var itemBox = document.createElement("div");
         itemBox.setAttribute("class","selBox");
