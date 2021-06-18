182ac92df36545243aadb80f67b29193242cc719
JorWo <42422209+JorWo@users.noreply.github.com>
2021-06-17 17:23:49 -1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const { readFile } = require('fs').promises;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Order = require('./models/order');
+var tables = 10;
 
 //Connect to mongodb database
 mongoose.connect('mongodb+srv://jordini:linguini@cluster0.7lkyv.mongodb.net/Cluster0?retryWrites=true&w=majority', {
@@ -11,14 +11,32 @@ mongoose.connect('mongodb+srv://jordini:linguini@cluster0.7lkyv.mongodb.net/Clus
     useUnifiedTopology: true
 });
 
+//Set view engine
+app.set('view engine', 'ejs');
+
 //Load website
 app.use(express.static('website'));
-app.use(bodyParser.json());
-app.get('/', async (req, res) => {
-   res.send(await readFile('./website/index.html', 'utf8'));
+app.get('/', (req,res) => {
+    res.render('index', { tableNum: req.query.table });   
+});
+app.get('/cart', (req,res) => {
+    res.render('cart', { tableNum: req.query.table });
+});
+app.get('/beverages', (req,res) => {
+    res.render('beverages', { tableNum: req.query.table });
+});
+app.get('/appetizers', (req,res) => {
+    res.render('appetizers', { tableNum: req.query.table });
+});
+app.get('/entrees', (req,res) => {
+    res.render('entrees', { tableNum: req.query.table });
+});
+app.get('/desserts', (req,res) => {
+    res.render('desserts', { tableNum: req.query.table });
 });
 
 //Fetch order from website and insert into mongodb
+app.use(bodyParser.json());
 app.post('/api/order', async (req,res) => {
     console.log(req.body);
     const {tableNum, order, subtotal, total} = req.body;
@@ -32,6 +50,18 @@ app.post('/api/order', async (req,res) => {
 
     res.json({status: 'ok'});
 })
+
+//Load kitchen view and its orders
+app.get('/kitchen', async (req,res) =>{
+    Order.find({}, (err,data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('kitchen', { tableOrder: data });
+        }
+    })
+});
 
 //Load server onto a port
 app.listen(process.env.PORT || 3000, () => console.log('App avaiable on http://localhost:3000'));
