7afaa7481e5a2b68cc7835feffb66ff7212e0617
JorWo <42422209+JorWo@users.noreply.github.com>
2021-06-27 17:31:50 -1000

    Add files via upload

--- models/order.js
+++ models/order.js
@@ -4,7 +4,8 @@ const orderSchema = new mongoose.Schema({
     tableNum: {type: Number, required: true},
     order: {type: String, required: true},
     subtotal: {type: String, required: true},
-    total: {type: String, required: true}
+    total: {type: String, required: true},
+    status: {type: String, required: true}
 }, {timestamps: true});
 
 const Order = mongoose.model('Order', orderSchema);
